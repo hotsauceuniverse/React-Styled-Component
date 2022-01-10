ad67dccb37e23f4f527330977762497bd152f60e
hotsauceuniverse <jangsy6976@naver.com>
2022-01-10 16:47:06 +0900

    Styled Component

---
+++ src/components/StyledA.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+
+const StyledA = styled.a.attrs((props) => ({
+  target: '_blank'
+}))`
+  color: ${(props) => props.color};
+`;
+
+export default StyledA
